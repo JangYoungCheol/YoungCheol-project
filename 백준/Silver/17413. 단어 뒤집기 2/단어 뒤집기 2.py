65a97260d070d029fb064876bb76f4eead8e980b
JangYoungCheol <1231yw1103@naver.com>
2026-01-28 11:19:10 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 100 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,41 @@
+# 알파벳, 숫자 < >로 구성
+# < > 안에 들어간 문자열은 뒤집지 말고 
+# 나머지는 다 뒤집는다.
+
+
+S = input()
+
+stack = []
+result = []
+in_tag = False
+
+for i in S:
+    if i == '<':
+        # <을 만나게 되면 stack에 있는 말 다 뒤집기
+        while stack:
+            print(stack.pop(), end='')
+
+        # < > 안에 있는 말은 어떻게 해야할지 몰라서 제미나이 도움 받음
+        in_tag = True
+        print(i, end ='')
+
+    elif i == '>':
+        in_tag = False
+        print(i, end='')
+
+    elif in_tag:
+        print(i, end ='')
+        
+    else:
+        if i == ' ':
+            while stack:
+
+                print(stack.pop(), end='')
+            print(' ', end='')
+        else:
+            stack.append(i)
+
+while stack:
+    print(stack.pop(), end = '')
+
+
