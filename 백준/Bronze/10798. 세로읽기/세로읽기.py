f348a1369412b3f4fe2d46d6eba12f566692db34
JangYoungCheol <1231yw1103@naver.com>
2026-02-18 20:06:58 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,10 @@
+words = [input() for _ in range(5) ]
+
+
+for j in range(15):
+    for i in range(5):
+        if j < len(words[i]) :
+            words.append(i)
+            print(words[i][j], end='')
+        
+print()
